8cf8e89f9bf73743b1ca1bd662c0f3095e0554c5
Malakghannam02 <ghannammalak222@gmail.com>
2025-01-09 20:22:23 +0300

    i have add hello all

---
+++ app.js
@@ -0,0 +1,9 @@
+let mystudents =["ahmad","anas","ali",["widad","layan","esraa"]];
+for (let i = 0; i < 3; i++) {
+    console.log(mystudents[i].toUpperCase())
+}
+
+for (let j = 0; j < mystudents[3].length; j++) {
+    console.log(mystudents[3][j].toUpperCase())
+
+}
